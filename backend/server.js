718ab4cbba9b6f154626f1ca4aa32aa0ccb20d48
OnlySarthak <devsarthak.hotline@gmail.com>
2026-03-28 19:27:25 +0530

    cors policy update 2

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,21 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "https://linkedin-assignment-taskmngr-git-main-onlysarthaks-projects.vercel.app",
+  "https://linkedin-assignment-taskmngr-2ttku6hlb-onlysarthaks-projects.vercel.app",
+  "https://linkedin-assignment-taskmngr-l2487jzk1-onlysarthaks-projects.vercel.app",
+  "http://localhost:5173"
+];
 
 app.use(cors({
-  origin: "https://linkedin-assignment-taskmngr-2ttku6hlb-onlysarthaks-projects.vercel.app",
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
