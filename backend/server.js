4dd85f75fbfb517d9550a15cd7a9bb3dca4d8901
OnlySarthak <devsarthak.hotline@gmail.com>
2026-03-28 18:02:02 +0530

    cors policy update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://linkedin-assignment-taskmngr.vercel.app/',
   credentials: true,
 }));
 
