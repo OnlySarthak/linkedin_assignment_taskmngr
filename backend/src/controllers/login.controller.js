1ec0fc578fa9903226ae26afcb2c024554519d5a
OnlySarthak <devsarthak.hotline@gmail.com>
2026-03-28 20:30:25 +0530

    maybe not last

--- backend/src/controllers/login.controller.js
+++ backend/src/controllers/login.controller.js
@@ -43,7 +43,7 @@ const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: true,
             sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             path: '/'
