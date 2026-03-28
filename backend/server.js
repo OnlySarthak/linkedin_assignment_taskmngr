b4ccff6805feef5c13eaa51cc7149fbf2d2dc1b4
OnlySarthak <devsarthak.hotline@gmail.com>
2026-03-28 19:19:25 +0530

    min changes

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 import router from './src/routers/routers.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require("cors");
 
 app.use(cors({
   origin: "https://linkedin-assignment-taskmngr-2ttku6hlb-onlysarthaks-projects.vercel.app",
